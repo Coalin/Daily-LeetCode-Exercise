1290a449b3d0dd5e70c8dc604328b51dd7b448b5
Colin <colinfdu@gmail.com>
2018-05-12 13:26:59 +0800

    Create 62_Unique-Paths.py

---
+++ 62_Unique-Paths.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m < 1 or n < 1:
+            return 0
+        dp = [[0]*n]*m
+        for i in xrange(m):
+            dp[i][0] = 1
+        for j in xrange(n):
+            dp[0][j] = 1
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+    
+                
+        
