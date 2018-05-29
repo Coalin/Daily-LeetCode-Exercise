de4ed2cee317958bffe3494e0303ebd9f2381225
Colin <colinfdu@gmail.com>
2018-05-29 10:08:20 +0800

    Update 94_Binary-Tree-Inorder-Traversal.py

--- 94_Binary-Tree-Inorder-Traversal.py
+++ 94_Binary-Tree-Inorder-Traversal.py
@@ -21,3 +21,21 @@ class Solution(object):
             self.recursive(root.left, res)
             res.append(root.val)
             self.recursive(root.right, res)
+
+            
+ # Solution II: Iteration
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
