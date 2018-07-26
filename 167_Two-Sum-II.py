e8b3ef802ddfd34f07d999a06fb8c2e82ccf0d50
Colin <colinfdu@gmail.com>
2018-07-26 10:56:17 +0800

    Create 167_Two-Sum-II.py

---
+++ 167_Two-Sum-II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i = 0
+        j = len(numbers)-1
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            else:
+                j -= 1
