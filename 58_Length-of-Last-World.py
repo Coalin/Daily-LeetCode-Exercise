ad204720b3a9556aa65b1a39574ef076463e5cb8
Colin <colinfdu@gmail.com>
2018-07-30 18:18:11 +0800

    Create 58_Length-of-Last-World.py

---
+++ 58_Length-of-Last-World.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        str = ''
+        length = 0
+        if len(s) == 0:
+            return 0
+        for i in reversed(s):
+            if i == ' ' and str != '':
+                return length
+            if i != ' ':
+                length += 1
+                str += i 
+        return length
+        
+   # 注意会有形如“a "的测试用例。
