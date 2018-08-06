24a7b9476c65fab2e5ed16b3d5849c6e49c7c51e
Colin <colinfdu@gmail.com>
2018-08-06 11:07:26 +0800

    Create 70_Climbing-Stairs.py

---
+++ 70_Climbing-Stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        dp = [0 for col in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1]
