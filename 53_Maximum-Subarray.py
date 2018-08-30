a8824d8a3b1107532d736c7fcbc8c538a501ac9d
Colin <colinfdu@gmail.com>
2018-08-30 09:35:58 +0800

    Update 53_Maximum-Subarray.py

--- 53_Maximum-Subarray.py
+++ 53_Maximum-Subarray.py
@@ -10,3 +10,15 @@ class Solution(object):
             cursum = max(num, cursum+num)
             maxsum = max(cursum, maxsum)
         return maxsum
+    
+ class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [-10086 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1]+nums[i], nums[i])
+        return max(dp)
