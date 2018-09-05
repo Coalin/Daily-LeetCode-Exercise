52da4b78058350dba788880cd6ecf2b251a1d384
Colin <colinfdu@gmail.com>
2018-09-05 14:07:00 +0800

    Create 31_Next-Permutation.py

---
+++ 31_Next-Permutation.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        
+# Attempt I: Failed
+
+        # if len(nums) > 1:
+        #     num_dict = dict()
+        #     for i in reversed(range(len(nums))):
+        #         for j in reversed(range(i)):
+        #             if nums[j] < nums[i]:
+        #                 num_dict[i] = j
+        #                 break
+        #     left = -1
+        #     for index in num_dict:
+        #         left = max(num_dict[index], left)
+        #     right = 0
+        #     for index_ in num_dict:
+        #         if num_dict[index_] == left:
+        #             right = index_
+        #     if right:
+        #         middle = nums[right]
+        #         for i in reversed(range(left, right)):
+        #             nums[i+1] = nums[i]
+        #         nums[left] = middle
+        
+# Attempt II: Accepted:
+
+        if len(nums) > 1:
+            pos = -1
+            for i in reversed(range(len(nums))):
+                if nums[i-1] < nums[i]:
+                    pos = i
+                    break
+            if pos != -1:
+                for j in reversed(range(len(nums))):
+                    if nums[j] > nums[pos-1]:
+                        nums[pos-1], nums[j] = nums[j], nums[pos-1]
+                        break
+                nums[pos:] = sorted(nums[pos:])
+            else:
+                nums.sort()
+        
