5bfa3929a9e28f8dfc2d0a5527bfa4ce5543d31d
Colin <colinfdu@gmail.com>
2018-09-16 22:05:34 +0800

    Update 21_Merge-Two-Sorted-Lists.py

--- 21_Merge-Two-Sorted-Lists.py
+++ 21_Merge-Two-Sorted-Lists.py
@@ -1,28 +1,30 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
+class Solution:
     def mergeTwoLists(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-        head = ListNode(0)
-        first = head
-        while l1 != None and l2 != None:
-            if l1.val > l2.val:
-                head.next = l2
-                l2 = l2.next
-            else:
-                head.next = l1
+        new_head = ListNode(0)
+        new_list = new_head
+        while l1 and l2:
+            if l1.val < l2.val:
+                new_head.next = l1
                 l1 = l1.next
-            head = head.next
-        if l1 == None:
-            head.next = l2
-        elif l2 == None:
-            head.next = l1
-        return first.next
+            else:
+                new_head.next = l2
+                l2 = l2.next
+            new_head = new_head.next
+        if l1:
+            new_head.next = l1
+        if l2:
+            new_head.next = l2
+        return new_list.next
+            
+# 66.35%; 64ms.  
