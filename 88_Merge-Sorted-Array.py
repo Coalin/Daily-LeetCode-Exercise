c68d0d578aa3d535a74942ffc4e8841b6ae72479
Colin <colinfdu@gmail.com>
2018-09-30 21:29:02 +0800

    Update 88_Merge-Sorted-Array.py

--- 88_Merge-Sorted-Array.py
+++ 88_Merge-Sorted-Array.py
@@ -1,3 +1,5 @@
+# Method I:
+
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
@@ -17,3 +19,28 @@ class Solution(object):
         nums1[m:] = nums2[:n]
         nums1.sort()
         """
+        
+# Method II:
+
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        k = m+n
+        while m and n:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[k-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[k-1] = nums2[n-1]
+                n -= 1
+            k -= 1
+        if n:
+            nums1[:n] = nums2[:n]
+            
+# 90.22%; 48ms.
