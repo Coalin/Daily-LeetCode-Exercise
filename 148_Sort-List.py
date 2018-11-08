16a648621f7cf7fe93a7a2775b2778b053f654d8
Colin <colinfdu@gmail.com>
2018-11-08 15:15:47 +0800

    Update 148_Sort-List.py

--- 148_Sort-List.py
+++ 148_Sort-List.py
@@ -1,3 +1,5 @@
+# Method I:
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -25,6 +27,61 @@ class Solution(object):
             head = head.next
         head.next = None
         return new_head
+
+# method II: Merge Sort
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        middle = self.findMiddle(head)
+        left = head
+        right = middle.next
+        middle.next = None
+        return self.mergeTwoLists(self.sortList(left), self.sortList(right))
+        
+        
+    def findMiddle(self, head):
+        fast = slow = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
+        
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        new_head = ListNode(0)
+        stored_head = new_head
+        while l1 and l2:
+            if l1.val<=l2.val:
+                new_head.next = l1
+                l1 = l1.next
+            else:
+                new_head.next = l2
+                l2 = l2.next
+            new_head = new_head.next            
+        if l1:
+            new_head.next = l1
+        if l2:
+            new_head.next = l2
+        return stored_head.next
+        
+        
             
         
         
