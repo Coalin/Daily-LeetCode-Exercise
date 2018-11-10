1d0a39758507ce28325527e0709ef6a67e68f0f3
Colin <colinfdu@gmail.com>
2018-11-10 16:48:22 +0800

    Create 215_Kth-Largest-Element-in-an-Array.py

---
+++ 215_Kth-Largest-Element-in-an-Array.py
@@ -0,0 +1,14 @@
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums = [-x for x in nums]
+        heapq.heapify(nums)
+        for _ in range(k):
+            res = heapq.heappop(nums)
+        return -res
