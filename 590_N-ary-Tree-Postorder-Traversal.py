e025a7a69a9347b7c5c7dcd6326b084702fe3db6
Colin <colinfdu@gmail.com>
2018-12-29 17:28:43 +0800

    Update 590_N-ary-Tree-Postorder-Traversal.py

--- 590_N-ary-Tree-Postorder-Traversal.py
+++ 590_N-ary-Tree-Postorder-Traversal.py
@@ -19,4 +19,24 @@ class Solution(object):
                 self.postorder(i)
             self.res.append(root.val)
         return self.res
+    
+class Solution(object):
+    def __init__(self):
+        self.res = []
+        self.stack = []
+    
+    def postorder(self, root):
+        if not root:
+            return []
+        
+        self.stack.append(root)
+        
+        while self.stack:
+            node = self.stack.pop()
+            self.res.append(node.val)
+            if node.children:
+                for i in node.children:
+                    self.stack.append(i)
+        
+        return self.res[::-1]
         
