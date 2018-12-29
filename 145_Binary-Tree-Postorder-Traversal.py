5e480b2241fc7590d9477210a4e4103c0968257a
Colin <colinfdu@gmail.com>
2018-12-29 17:21:10 +0800

    Update 145_Binary-Tree-Postorder-Traversal.py

--- 145_Binary-Tree-Postorder-Traversal.py
+++ 145_Binary-Tree-Postorder-Traversal.py
@@ -19,6 +19,26 @@ class Solution(object):
             self.postorderTraversal(root.right)
             self.res.append(root.val)
         return self.res
+    
+class Solution(object):
+    def __init__(self):
+        self.res = []
+        self.stack = []
+    
+    def postorderTraversal(self, root):
+        if not root:
+            return []
+        
+        self.stack.append(root)
+        while self.stack:
+            node = self.stack.pop()
+            self.res.append(node.val)
+            if node.left:
+                self.stack.append(node.left)
+            if node.right:
+                self.stack.append(node.right)
+        
+        return self.res[::-1]
             
         
         
