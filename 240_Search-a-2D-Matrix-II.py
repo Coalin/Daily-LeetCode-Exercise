69b9f3a9d2851ea6613de1e867776dc2b6f84796
Colin <colinfdu@gmail.com>
2019-01-07 12:45:22 +0800

    Update 240_Search-a-2D-Matrix-II.py

--- 240_Search-a-2D-Matrix-II.py
+++ 240_Search-a-2D-Matrix-II.py
@@ -31,5 +31,32 @@ class Solution:
                     break
         
         return False
+
+
+# Two Pointers
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        if not matrix[0]:
+            return False
+        
+        i = 0
+        j = len(matrix[0])-1
+        
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+                
+        return False
         
         
