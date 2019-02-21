f4dffae46b668a607bea18a83c3aa6b1526a1dc2
Colin <colinfdu@gmail.com>
2019-02-21 11:48:21 +0800

    Update 7_Reverse-Integer.py

--- 7_Reverse-Integer.py
+++ 7_Reverse-Integer.py
@@ -30,3 +30,18 @@ class Solution:
                 return -int(res)
             else:
                 return 0
+    
+    
+# Exercise II:
+class Solution:
+    def reverse(self, x: 'int') -> 'int':
+        res = int(str(abs(x))[::-1])
+        if res > 2**31-1:
+            return 0
+        if x < 0:
+            return res*(-1)
+        elif x == 0:
+            return 0
+        else:
+            return res
+        
