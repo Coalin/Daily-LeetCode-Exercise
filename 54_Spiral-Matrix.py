f5acbe2f5cca2295bf375756afd7bffbf2a69e59
Colin <colinfdu@gmail.com>
2019-03-19 16:49:18 +0800

    Update 54_Spiral-Matrix.py

--- 54_Spiral-Matrix.py
+++ 54_Spiral-Matrix.py
@@ -61,5 +61,59 @@ class Solution(object):
             colBegin += 1
             
         return res
+    
+# Exercise II
+# Mar 19, 2019
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:              
+        def cycle(row, col, ri, ci, case):
+            if row == 0 or col == 0:
+                return 
+            
+            if case == 0:
+                for i in range(ci, ci+col):
+                    res.append(matrix[ri][i])
+                ri += 1
+                ci = ci+col-1
+                row -= 1
+                case = (case+1)%4
+                cycle(row, col, ri, ci, case)
+                
+            elif case == 1:
+                for i in range(ri, ri+row):
+                    res.append(matrix[i][ci])
+                ri = ri+row-1
+                ci -= 1
+                col -= 1
+                case = (case+1)%4
+                cycle(row, col, ri, ci, case)
+                
+            elif case == 2:
+                for i in reversed(range(ci-col+1, ci+1)):
+                    res.append(matrix[ri][i])
+                ri -= 1
+                ci = ci-col+1
+                row -= 1
+                case = (case+1)%4
+                cycle(row, col, ri, ci, case)
+
+            elif case == 3:
+                for i in reversed(range(ri-row+1, ri+1)):
+                    res.append(matrix[i][ci])
+                ri = ri-row+1
+                ci += 1
+                col -= 1
+                case = (case+1)%4
+                cycle(row, col, ri, ci, case)
         
+        if not matrix:
+            return []
+        res = []
+        cycle(len(matrix), len(matrix[0]), 0, 0, 0)
+        return res
+        
+
+                
+                
+            
         
