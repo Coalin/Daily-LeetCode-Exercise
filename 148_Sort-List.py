bb8a6f28697b439b2173e7d7fbdd1a5e3ee684f1
Colin <colinfdu@gmail.com>
2019-04-16 21:43:43 +0800

    Update 148_Sort-List.py

--- 148_Sort-List.py
+++ 148_Sort-List.py
@@ -80,6 +80,61 @@ class Solution:
         if l2:
             new_head.next = l2
         return stored_head.next
+
+# Exercise III:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        return self.divide(head)
+        
+    def divide(self, head):
+        if not head.next:
+            return head
+        slow = fast = pre = head
+        while slow and fast and fast.next:
+            pre = slow
+            slow = slow.next
+            fast = fast.next.next
+        pre.next = None
+        p1 = self.divide(head)
+        p2 = self.divide(slow)
+        return self.combine(p1, p2)
+        
+    def combine(self, p1, p2):
+        if not p1:
+            return p2
+        if not p2:
+            return p1
+        
+        new_head = ListNode(0)
+        temp = new_head
+        
+        while p1 and p2:
+            if p1.val < p2.val:
+                new_head.next = p1
+                p1 = p1.next
+            else:
+                new_head.next = p2
+                p2 = p2.next
+            new_head = new_head.next
+            
+        if p1:
+            new_head.next = p1
+        if p2:
+            new_head.next = p2
+            
+        return temp.next
         
         
             
