ae30226d9e3939b80905b4c02f858e649d548549
Colin <colinfdu@gmail.com>
2019-05-03 13:59:48 +0800

    Create 492_Construct-the-Rectangle.py

---
+++ 492_Construct-the-Rectangle.py
@@ -0,0 +1,19 @@
+import math
+
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        if area == 0 or area != int(area):
+            return None
+        
+        length = math.sqrt(area)
+        if length == int(length):
+            return [int(length), int(length)]
+        
+        w = round(length)
+        # print(w)
+        while w > 0:
+            if area%w == 0:
+                # print('flag')
+                return [area//w, w]
+            else:
+                w -= 1
