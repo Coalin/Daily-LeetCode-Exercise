c17ab64f0e0a9a17d171da94c6678043208769ae
Colin <colinfdu@gmail.com>
2019-05-04 14:58:06 +0800

    Update 485_Max-Consecutive-Ones.py

--- 485_Max-Consecutive-Ones.py
+++ 485_Max-Consecutive-Ones.py
@@ -1,3 +1,4 @@
+# Exercise I:
 class Solution:
     def findMaxConsecutiveOnes(self, nums):
         """
@@ -16,4 +17,29 @@ class Solution:
             total_len = max(total_len, cur_len)
         return total_len
             
+     
+# Exercise II:
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
         
+        res = 0
+        cur_res = 0
+        
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                res = max(cur_res, res)
+                cur_res = 0
+            else:
+                cur_res += 1
+            
+        res = max(cur_res, res)
+                
+        return res
+    
+ 
+# Exercise III:
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        return max([len(i) for i in "".join([str(i) for i in nums]).split("0")])
