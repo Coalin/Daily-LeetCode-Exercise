6c99fa3ec701d9233fac79a98a629be27e8c37c2
Colin <colinfdu@gmail.com>
2020-04-10 23:23:55 +0800

    Update 151_Reverse-Words-in-a-String.py

--- 151_Reverse-Words-in-a-String.py
+++ 151_Reverse-Words-in-a-String.py
@@ -11,4 +11,33 @@ class Solution(object):
             res += ' ' + word_list.pop().strip()
         return res.strip()
     
+    
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s_nb = s.strip()
+        stack = []
+        cur_word = ''
+
+        i = 0
+        while i < len(s_nb):
+            if s_nb[i] != ' ':
+                cur_word += s_nb[i]
+            else:
+                if cur_word != '':
+                    stack.append(cur_word)
+                cur_word = ''
+            i += 1
+        
+        res = cur_word+' '
+        while stack:
+            res += stack.pop()
+            res += ' '
+        
+        return res.strip() 
+
+    
         
