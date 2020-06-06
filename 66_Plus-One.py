1f6bea2c243ed6247c2806c8d5090029c7d1e889
Colin <colinfdu@gmail.com>
2020-06-06 12:10:41 +0800

    Create 66_Plus-One.py

---
+++ 66_Plus-One.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if not digits:
+            return []
+        
+        for i in reversed(range(len(digits))):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+
+        return [1]+[0]*len(digits)
