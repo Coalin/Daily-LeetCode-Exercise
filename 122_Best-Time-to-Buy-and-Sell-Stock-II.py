ca8ccedd6109ef1d4c1cfe4fa52bfed2b0d8cde7
Colin <zjecust@foxmail.com>
2020-11-08 14:06:20 +0800

    LC 122

--- 122_Best-Time-to-Buy-and-Sell-Stock-II.py
+++ 122_Best-Time-to-Buy-and-Sell-Stock-II.py
@@ -1,31 +1,26 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        if len(prices) == 0:
-            return 0
-        last = prices[0]
-        res = 0
-        for i in prices:
-            if i > last:
-                res += i-last
-            last = i
-        return res
-        
+# 遍历
 # 总利润：把每两天的价格增长量加起来
-
-# Exercise II
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        if len(prices) <= 1:
-            return 0
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         res = 0
-        for i in range(1, len(prices)):
-            res += max(prices[i]-prices[i-1], 0)
+        if len(prices) >= 2:
+            for i in range(1, len(prices)):
+                if prices[i] > prices[i-1]:
+                    res += prices[i]-prices[i-1]
         return res
+
+
+# 动态规划
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [[0 for _ in range(2)] for _ in range(len(prices))]
+
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+
+        if len(prices) >= 2:
+            for i in range(1, len(prices)):
+                dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+                dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+
+        return dp[-1][0]
