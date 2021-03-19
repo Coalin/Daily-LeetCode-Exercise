ba0940110d2bfc8e5881d31d3eb050fbb7709f31
Colin <zjecust@foxmail.com>
2021-03-19 14:42:01 +0800

    LC 92

--- 92_Reverse-Linked-List-II.py
+++ 92_Reverse-Linked-List-II.py
@@ -72,5 +72,50 @@ class Solution(object):
             return p1
         else:
             return pre
+
+
+# Exercise III:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        if not head:
+            return 
+
+        pre_node = ListNode(0)
+        cur_node = ListNode(0)
+        reset_node_1 = ListNode(0)
+        reset_node_2 = ListNode(0)
+        res_node = ListNode(0)
+
+        cur_node.next = head
+        pre_node.next = head
+        res_node.next = head
+        reset_node_1.next = head
+
+        for _ in range(left):
+            cur_node = cur_node.next
         
-        
+        for _ in range(left-1):
+            pre_node = pre_node.next
+
+        for _ in range(left-1):
+            reset_node_1 = reset_node_1.next
+
+        reset_node_2 = reset_node_1.next 
+
+        for _ in range(left-1, right):
+            next_node = cur_node.next 
+            cur_node.next = pre_node
+            pre_node = cur_node
+            cur_node = next_node
+
+        reset_node_1.next = pre_node 
+        reset_node_2.next = cur_node
+
+        if left == 1:
+            return reset_node_1.next 
+        return res_node.next
