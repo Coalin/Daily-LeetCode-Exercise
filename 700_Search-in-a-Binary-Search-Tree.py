29d9c2a1c872d6537eaed4c3bb2fb1db136d5c53
Colin <zjecust@foxmail.com>
2021-11-28 12:14:56 +0800

    LC 700

---
+++ 700_Search-in-a-Binary-Search-Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        if root.val < val:
+            return self.searchBST(root.right, val)
