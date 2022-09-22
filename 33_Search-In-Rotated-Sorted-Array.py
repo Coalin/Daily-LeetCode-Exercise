e059aa10b67808d4ea2ede104b708a726b2d783e
Colin <zjecust@foxmail.com>
2022-09-22 21:07:58 +0800

    20220922

--- 33_Search-In-Rotated-Sorted-Array.py
+++ 33_Search-In-Rotated-Sorted-Array.py
@@ -66,4 +66,33 @@ class Solution:
             
         return -1
                 
-    
+# Exercise III
+# Date: Sep 22, 2022    
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+
+        while left < right:
+            mid = left + (right-left)//2
+            if nums[left] == target:
+                return left 
+            if nums[right] == target:
+                return right
+            if nums[mid] == target:
+                return mid 
+            if nums[mid] > nums[left]:
+                if target > nums[left] and target < nums[mid]:
+                    right = mid-1 
+                else:
+                    left = mid+1
+            else:
+                if target > nums[mid] and target < nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
+
+        if nums[left] == target:
+            return left 
+        
+        return -1
