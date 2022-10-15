cd98e311f5262a14c4741bd6789922d2101040ad
Colin <zjecust@foxmail.com>
2022-10-15 18:01:45 +0800

    20221015

--- 5_Longest-Palindromic-Substring.py
+++ 5_Longest-Palindromic-Substring.py
@@ -70,3 +70,29 @@ class Solution:
                         
         return s[L:R+1]
                 
+
+# Exercise III:
+# Oct 15, 2022
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        left = right = 0
+
+        for i in range(len(s)):
+            l = r = i 
+            while l >= 0 and r <= len(s)-1 and s[l] == s[r]:
+                l -= 1
+                r += 1
+            if r-l-2 > right-left:
+                left = l+1 
+                right = r-1
+            
+            l = i 
+            r = i+1 
+            while l >= 0 and r <= len(s)-1 and s[l] == s[r]:
+                l -= 1 
+                r += 1 
+            if r-l-2 > right-left:
+                left = l+1 
+                right = r-1
+
+        return s[left:right+1]            
