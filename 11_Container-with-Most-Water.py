937ac01dc4a88df20f6db8f9e3a5898c3ab838a3
Colin <zjecust@foxmail.com>
2022-10-31 11:18:13 +0800

    20221031

--- 11_Container-with-Most-Water.py
+++ 11_Container-with-Most-Water.py
@@ -18,3 +18,16 @@ class Solution:
 
 # 33.15%; 120ms. 
         
+
+# Exercise II: 20221031
+class Solution:
+    # 每个位置的容量始终受左右两端范围内(不是相邻)最高高度的的较低高度控制
+    def trap(self, height: List[int]) -> int:
+        res = 0 
+        left_high = height[0]
+        right_high = height[-1]
+        for i in range(1, len(height)-1):
+            left_high = max(left_high, height[i-1])
+            right_high = max(height[i+1:])
+            res += max((min(left_high, right_high)-height[i]), 0)
+        return res
