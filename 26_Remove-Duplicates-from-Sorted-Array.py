f01f5abad8ad8c39a969ce006e7ccbe71867a611
colin.zj <zjecust@foxmail.com>
2023-01-07 00:58:29 +0800

    LC 20230107

--- 26_Remove-Duplicates-from-Sorted-Array.py
+++ 26_Remove-Duplicates-from-Sorted-Array.py
@@ -15,6 +15,18 @@ class Solution(object):
       
 # 98.99%; 56ms.
                 
-        
+
+# Exercise II:
+# Jan 7, 2023
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0 
+        j = 1
+
+        while j < len(nums):
+            if nums[i] != nums[j]:
+                nums[i+1] = nums[j]
+                i += 1
+            j += 1
             
-        
+        return i+1        
