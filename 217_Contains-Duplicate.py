5aa267b699b5cc8fa8ffe0109f2c7e559b2d9a13
colin.zj <zjecust@foxmail.com>
2023-01-09 14:46:58 +0800

    LC 20230109

--- 217_Contains-Duplicate.py
+++ 217_Contains-Duplicate.py
@@ -16,4 +16,18 @@ class Solution:
             if num_dict[i] >= 2:
                 return True
         return False
-        
+
+
+# Exercise II:
+# Jan 9, 2023
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        res_dic = {}
+
+        for i in range(len(nums)):
+            if nums[i] in res_dic:
+                return True 
+            else:
+                res_dic[nums[i]] = 1
+
+        return False
