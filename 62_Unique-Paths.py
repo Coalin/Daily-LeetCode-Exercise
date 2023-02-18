0e0718f704435cc4fa142fdf56ee1348231f2a0d
colin.zj <zjecust@foxmail.com>
2023-02-18 10:59:36 +0800

    LC 20230218

--- 62_Unique-Paths.py
+++ 62_Unique-Paths.py
@@ -21,4 +21,14 @@ class Solution(object):
 # 95.75%; 44ms. 
     
                 
+# Exercise II:
+# Feb 18, 2023
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for _ in range(n)] for _ in range(m)]
         
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+
+        return dp[-1][-1]        
--- 63_Unique-Paths-II.py
+++ 63_Unique-Paths-II.py
@@ -31,4 +31,34 @@ class Solution(object):
                 else:
                     dp[i][j] = 0
         return dp[m-1][n-1]
-                    
+                
+
+# Exercise II:
+# Feb 18, 2023
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m = len(obstacleGrid)
+        n  = len(obstacleGrid[0])
+
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+
+        for x in range(m):
+            for y in range(n):
+                if obstacleGrid[x][y] == 1:
+                    dp[x][y] = 0
+
+        for p in range(1, m):
+            if obstacleGrid[p][0] != 1:
+                dp[p][0] = dp[p-1][0]
+
+        for q in range(1, n):
+            if obstacleGrid[0][q] != 1:
+                dp[0][q] = dp[0][q-1]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] != 1:
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1]
+
+        return dp[-1][-1]
+    
