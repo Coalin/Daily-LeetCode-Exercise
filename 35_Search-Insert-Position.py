1df9813902e050a42740443b7d43433e1319c4e2
colin.zj <zjecust@foxmail.com>
2023-03-09 13:27:11 +0800

    LC 20230309

--- 35_Search-Insert-Position.py
+++ 35_Search-Insert-Position.py
@@ -21,3 +21,31 @@ class Solution(object):
             return left+1
         else:
             return left
+
+
+# Exercise II:
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0 
+        right = len(nums)-1
+
+        while left < right:
+            mid = left + (right-left)//2
+
+            if nums[left] == target:
+                return left 
+            if nums[right] == target:
+                return right 
+            if nums[mid] == target:
+                return mid 
+
+            if nums[mid] < target:
+                left = mid+1 
+            else:
+                right = mid-1 
+
+        if nums[left] < target:
+            return left+1
+        else:
+            return left
+
