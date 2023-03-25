52f97d0b1b7074ef6023acce46ab9e04bd169a62
colin.zj <zjecust@foxmail.com>
2023-03-25 12:12:57 +0800

    LC 20230325

--- 98_Validate-Binary-Search-Tree.py
+++ 98_Validate-Binary-Search-Tree.py
@@ -25,5 +25,29 @@ class Solution:
         return sorted(self.res) == self.res and len(set(self.res)) == len(self.res)
 
         
+# Exercise II:
+# Mar 25, 2023
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        res = []
+        self.inorder(root, res)
 
+        for i in range(1, len(res)):
+            if res[i] <= res[i-1]:
+                return False
         
+        return True
+
+    def inorder(self, root, res):
+        if not root:
+            return 
+
+        self.inorder(root.left, res)
+        res.append(root.val)
+        self.inorder(root.right, res)
