9879d6b1c49e18bb13233eb60b19d3bda74d71c5
colin.zj <zjecust@foxmail.com>
2023-09-28 23:31:59 +0800

    20230928

--- 198_House-Robber.py
+++ 198_House-Robber.py
@@ -33,3 +33,22 @@ class Solution:
             dp[i][1] = max(dp[i-1][0]+nums[i], dp[i-1][1])
 
         return max(dp[-1][0], dp[-1][1])
+
+
+# Exercise III
+# Sep 28, 2023
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+
+        max_amt = [0 for _ in range(len(nums))]
+        max_amt[0] = nums[0]
+        max_amt[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            max_amt[i] = max(max_amt[i-2]+nums[i], max_amt[i-1])
+
+        return max_amt[-1]
--- 746_Min-Cost-Climbing-Stairs.py
+++ 746_Min-Cost-Climbing-Stairs.py
@@ -17,3 +17,21 @@ class Solution(object):
                 cur_num = min(res[i-2]+cost[i-2], res[i-1]+cost[i-1])
                 res.append(cur_num)
         return min(res[-2]+cost[-2], res[-1]+cost[-1])
+
+
+# Exercise II
+# Sep 28, 2023
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        if len(cost) <= 1:
+            return 0
+        if len(cost) == 2:
+            return min(cost[0], cost[1])
+
+        dispense_min = [2**32 for _ in range(len(cost))]
+        dispense_min[0] = 0
+        dispense_min[1] = 0
+        
+        for i in range(2, len(cost)):
+            dispense_min[i] = min(dispense_min[i-1]+cost[i-1], dispense_min[i-2]+cost[i-2])
+        return min(dispense_min[-1]+cost[-1], dispense_min[-2]+cost[-2])
