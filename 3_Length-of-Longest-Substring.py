2ba1e12e57a2071d50b8cbdf56c356fdbeceb654
colin.zj <zjecust@foxmail.com>
2023-12-09 16:31:52 +0800

    20231209

--- 3_Length-of-Longest-Substring.py
+++ 3_Length-of-Longest-Substring.py
@@ -13,3 +13,27 @@ class Solution(object):
             d[s[i]] = i
             ans = max(ans, i-start+1)
         return ans
+
+
+# Exercise II:
+# Dec 9, 2023
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        res = 1 
+        cur_set = set()
+        left = 0
+        right = 0
+
+        if not s:
+            return 0
+
+        while right < len(s):
+            if s[right] not in cur_set:
+                cur_set.add(s[right])
+                res = max(res, len(cur_set))
+                right += 1
+            else:
+                cur_set.remove(s[left])
+                left += 1
+
+        return res
