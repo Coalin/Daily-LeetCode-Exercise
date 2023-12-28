95cfae2759b390134153e7a940ea7a1f8dbf1134
colin.zj <zjecust@foxmail.com>
2023-12-28 23:01:57 +0800

    20231228

--- 141_Linked-List-Cycle.py
+++ 141_Linked-List-Cycle.py
@@ -44,3 +44,28 @@ class Solution:
                 return True 
  
         return False
+
+
+# Exercise III:
+# Dec 28, 2023
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False 
+
+        fast = head.next
+        slow = head 
+
+        while fast != slow:
+            if not fast or not fast.next:
+                return False
+            fast = fast.next.next 
+            slow = slow.next 
+
+        return True
