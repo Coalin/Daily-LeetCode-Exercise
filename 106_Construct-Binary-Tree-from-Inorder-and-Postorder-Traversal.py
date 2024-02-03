5389f8d21b715962070ecead84d46916f09bef8e
colin.zj <zjecust@foxmail.com>
2024-02-03 15:32:23 +0800

    20240203

--- 106_Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal.py
+++ 106_Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal.py
@@ -21,3 +21,29 @@ class Solution:
         root.right = self.buildTree(inorder[index+1:], postorder[index:])
         return root
         
+
+# Exercise II:
+# Feb 3, 2024
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not postorder:
+            return
+
+        root = TreeNode(postorder[-1])
+        root_index = inorder.index(postorder[-1])
+        left_inorder = inorder[:root_index]
+        right_inorder = inorder[root_index+1:]
+
+        left_postorder = postorder[:len(left_inorder)]
+        right_postorder = postorder[len(left_inorder):len(postorder)-1]
+
+        root.left = self.buildTree(left_inorder, left_postorder)
+        root.right = self.buildTree(right_inorder, right_postorder)
+
+        return root
--- 149_Max-Points-on-a-Line.py
+++ 149_Max-Points-on-a-Line.py
@@ -41,6 +41,28 @@ class Solution(object):
             res = max(len(res_dic[index]), res)
         return res
             
-            
-        
-        
+
+# Exercise II:
+# Jan 31, 2024
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) == 1:
+            return 1
+
+        res = 1
+        for i in range(len(points)):
+            slope_dict = dict()
+            for j in range(len(points)):
+                if i != j:
+                    if points[i][0]-points[j][0] == 0:
+                        cur_slope = float('inf')
+                    else:
+                        cur_slope = (points[i][1]-points[j][1])/(points[i][0]-points[j][0])
+                    if cur_slope in slope_dict:
+                        slope_dict[cur_slope] += 1
+                    else:
+                        slope_dict[cur_slope] = 1
+                    cur_cnt = max(slope_dict.values())
+                    res = max(cur_cnt, res)
+
+        return res+1
