8418719596dd5bedb0874b7f2b36cf51b3234688
colin.zj <zjecust@foxmail.com>
2024-02-04 21:41:13 +0800

    20240204

--- 122_Best-Time-to-Buy-and-Sell-Stock-II.py
+++ 122_Best-Time-to-Buy-and-Sell-Stock-II.py
@@ -44,3 +44,15 @@ class Solution:
             dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
 
         return dp[-1][0]
+
+
+# Exercise IV:
+# Feb 4, 2024
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+
+        for i in range(1, len(prices)):
+            res += max(prices[i]-prices[i-1], 0)
+
+        return res
--- 236_Lowest-Common-Ancestor-of-a-Binary-Tree.py
+++ 236_Lowest-Common-Ancestor-of-a-Binary-Tree.py
@@ -85,3 +85,30 @@ class Solution:
             return right 
         
         return None
+
+
+# Exercise IV:
+# Feb 4, 2024
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root 
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root 
+        elif left:
+            return left 
+        else:
+            return right 
+
+        return None
